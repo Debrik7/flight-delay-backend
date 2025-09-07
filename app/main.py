69935e0d44215abbb8960d86ec08f916c5cbae12
Atyasha Bhattacharyya <139572321+atyasha2054@users.noreply.github.com>
2025-09-07 23:32:12 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.models.request_model import FlightData
-from app.services.predict import predict_delay
+from app.models.request_model import FlightInput
+from app.services.predict import predict_regression, predict_classification
 
 app = FastAPI(title="Flight Delay Prediction API")
 
-# Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this to your frontend domain
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,7 +17,23 @@ app.add_middleware(
 def home():
     return {"message": "Welcome to Flight Delay Prediction API"}
 
-@app.post("/predict")
-def predict(data: FlightData):
-    result = predict_delay(data)
-    return {"predicted_arrival_delay": result}
+@app.post("/predict/regression")
+def predict_reg(data: FlightInput):
+    try:
+        predicted_delay = predict_regression(data.dict())
+        return {"predicted_delay_minutes": predicted_delay}
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.post("/predict/classification")
+def predict_clf(data: FlightInput):
+    try:
+        try:
+            payload = data.model_dump()   # v2
+        except AttributeError:
+            payload = data.dict()         # v1
+
+        delay_probability = predict_classification(payload)
+        return {"delay_probability": delay_probability}
+    except Exception as e:
+        return {"error": str(e)}
